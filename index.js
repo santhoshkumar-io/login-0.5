bddf5caae2c18196ceef622b4389c021768b098d
santhoshkumar-io <santhoshkumars1903@gmail.com>
2024-12-30 18:32:23 +0530

    added footer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 class LoginController {
     constructor() {
@@ -24,16 +25,22 @@ class LoginController {
             document.location = './dashboard/dashboard.html';
         } catch (error) {
             this.displayErrorMessage(error);
+
         }
     };
     displayErrorMessage(err) {
         const errorMessage = document.getElementById("error");
         errorMessage.textContent = err;
-        console.log(errorMessage)
+        const loginForm = document.getElementById("login-form");
+        loginForm.uname.classList.add("is-invalid")
+        loginForm.password.classList.add("is-invalid")
+
     }
 }
 document.addEventListener("DOMContentLoaded", () => {
     const loginCon = new LoginController();
+    const unameErr = document.getElementById("uname-err");
+
     const accessToken = sessionStorage.getItem('accessToken');
     if (accessToken) {
         document.location = './dashboard/dashboard.html'
@@ -43,17 +50,48 @@ document.addEventListener("DOMContentLoaded", () => {
     loginForm.addEventListener("submit", async (event) => {
         event.preventDefault();
         await loginCon.validateUser(loginForm.uname.value, loginForm.password.value);
-    });
-    loginForm.uname.addEventListener('input', () => {
-        const regex = /^[a-z0-9]+$/;
-        if (!regex.test(loginForm.uname.value)) {
-
-            loginCon.displayErrorMessage("Username can only contain lowercase letters and numbers!");
-        } else {
-            loginCon.displayErrorMessage("");
+        if (!loginForm.checkValidity()) {
+            unameErr.textContent = "Required field!";
 
         }
     });
+    loginForm.uname.addEventListener('input', () => {
+        const unameErr = document.getElementById("uname-err");
+        const unameregex = /^[a-z0-9]+$/;
+        if (!unameregex.test(loginForm.uname.value)) {
+            loginForm.uname.classList.remove("is-valid")
+            loginForm.uname.classList.add("is-invalid")
+            unameErr.textContent = "Invalid username";
+            if (loginForm.uname.value == '') {
+                unameErr.textContent = "Required field!";
+
+            }
+
+        } else {
+            loginForm.uname.classList.remove("is-invalid")
+
+            loginForm.uname.classList.add("is-valid")
+            unameErr.textContent = "";
+        }
+
+    });
+    loginForm.password.addEventListener('input', () => {
+        const passErr = document.getElementById("pass-err");
+        const passRegex = /^.{8,16}$/;
+        if (!passRegex.test(loginForm.password.value)) {
+            loginForm.password.classList.remove("is-valid")
+            loginForm.password.classList.add("is-invalid")
+            passErr.textContent = "Too short or too long!";
+            if (loginForm.password.value == '') {
+                passErr.textContent = "Required field!";
+
+            }
+        } else {
+            loginForm.password.classList.remove("is-invalid")
+            loginForm.password.classList.add("is-valid")
+            passErr.textContent = "";
+        }
+    })
 
 });
 
